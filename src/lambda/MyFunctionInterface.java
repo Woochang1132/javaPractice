390c5d6b23e8f50fa2aba6632c6ce87d9a4272ab
Woochang1132 <show4031132@gmail.com>
2024-01-25 17:27:49 +0900

    java-Practice : 람다식 표현 예제

---
+++ src/lambda/MyFunctionInterface.java
@@ -0,0 +1,5 @@
+package lambda;
+
+public interface MyFunctionInterface {
+    public void method();
+}
---
+++ src/lambda/MyFunctionInterfaceExample.java
@@ -0,0 +1,7 @@
+package lambda;
+
+public class MyFunctionInterfaceExample {
+    public static void main(String[] args){
+        MyFunctionInterface fi;
+    }
+}
